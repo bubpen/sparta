69efb10b533857e60e5c44ed3222f4c051c00f90
bubpen <shghtjd711@naver.com>
2024-12-18 20:55:18 +0900

    modify supervised_learning.py

--- machine_learning/supervised_learning.py
+++ machine_learning/supervised_learning.py
@@ -7,16 +7,21 @@ from sklearn.metrics import mean_squared_error, r2_score
 df = pd.read_csv('housingdata.csv')
 print(df)
 df_median = df[:]
+
+# 결측치를 평균값으로 대체, CHAS 는 0 과 1로 구분되어 0으로 대체
 for i in df_median.columns:
     if df_median[i].isnull().sum() > 0:
         if i == 'CHAS':
             df_median[i] = df_median[i].fillna(0)
         else:
-            df_median[i] = df_median[i].fillna(round(df[i].mean()))
+            df_median[i] = df_median[i].fillna(df[i].mean())
+
 
 
 X = df_median[['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']]
 y = df_median['MEDV']
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35, random_state=42)
 
 # 다항 회귀 모델 생성 및 학습
