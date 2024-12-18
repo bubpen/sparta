3046cc5929c201eb865b7e63c53e22b3dd11ed8f
bubpen <shghtjd711@naver.com>
2024-12-18 14:05:46 +0900

    modify machine_learning

--- machine_learning/supervised_learning.py
+++ machine_learning/supervised_learning.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
-print()
+
 data_csv = pd.read_csv('housingdata.csv')
-print(data_csv.isna().sum(), data_csv.dtypes)
+#print(data_csv.isna().sum(), data_csv.dtypes)
 data_csv_median = data_csv.fillna(data_csv.median)
+print(data_csv.median)
+print("")
 print(data_csv_median)
+data_csv_without_na = data_csv.dropna()
+print(data_csv_without_na.columns)
